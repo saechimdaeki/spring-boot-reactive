baafddbc5285fd95de495b2bb8849c7d677609cf
saechimdaeki <anima94@kakao.com>
2021-08-08 12:10:16 +0900

    chapter05 project code setting and refactor

---
+++ chap05/src/main/java/com/saechimdaeki/chap05/ItemByExampleRepository.java
@@ -0,0 +1,10 @@
+
+
+package com.saechimdaeki.chap05;
+
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
+
+
+public interface ItemByExampleRepository extends ReactiveQueryByExampleExecutor<Item> {
+
+}
